f552ae2ab07e63dca4ce0586e6b9090e133d5fe1
Smit Roy <ttssmitherio@gmail.com>
2026-05-19 22:09:39 +0530

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+
+        Arrays.sort(charS);
+        Arrays.sort(charT);
+
+        
+        for(int i = 0; i < charS.length; i++){
+            if(charS[i]!=charT[i]){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
