169ca974f8ec6fff14a87c8b5e7122af84f86213
Smit Roy <smitroy22@gmail.com>
2026-06-10 08:05:14 +0530

    Add: valid-perfect-square - submission-0

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-0.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        for (long i = 1; i <= num; i++) {
+            long sq = i * i;
+            if (sq > num) {
+                return false;
+            }
+            if (sq == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
